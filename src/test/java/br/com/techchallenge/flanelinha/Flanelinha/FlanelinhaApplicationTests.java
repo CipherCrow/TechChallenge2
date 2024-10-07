8df855c4117af1a28e01beeb455f0e8ef66e2043
CipherCrow <jonastorquatro@gmail.com>
2024-10-07 01:08:20 -0300

    iniciando projeto

---
+++ src/test/java/br/com/techchallenge/flanelinha/Flanelinha/FlanelinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.flanelinha.Flanelinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlanelinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
